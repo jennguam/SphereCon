0126c01842401815e1bb856d208f05fb8858ff0f
Jenn Leung <j.leung91@gmail.com>
2015-08-24 23:40:11 -0700

    refactoring

--- js/main.js
+++ js/main.js
@@ -13,12 +13,12 @@ $(function() {
 
 // Floating label headings for the contact form
 $(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
+    $("body").on("input propertychange", ".floating-label-form", function(e) {
+        $(this).toggleClass("floating-label-form-with-value", !! $(e.target).val());
+    }).on("focus", ".floating-label-form", function() {
+        $(this).addClass("floating-label-form-with-focus");
+    }).on("blur", ".floating-label-form", function() {
+        $(this).removeClass("floating-label-form-with-focus");
     });
 });
 
